c19072689123e8f8c56f2e5e942b4432b7d194db
Qubue <j.wojcicki@yahoo.com>
2017-02-08 15:36:25 +0100

    Created ninject objects, resolve dependency, created Repository and IRepo

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type servicesType)
+        {
+            return kernel.GetAll(servicesType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m=>m.Products).Returns(new List<Product>{
+                new Product { Name = "Piłka nożna", Price = 25 },
+                new Product { Name = "Deska surfingowa", Price = 179 },
+                new Product { Name = "Buty do biegania", Price = 95 }
+            }.AsQueryable);
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
