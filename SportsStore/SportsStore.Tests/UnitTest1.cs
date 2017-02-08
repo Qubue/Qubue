9b2914fbd021195289587395b53e0c710f378d2c
Qubue <j.wojcicki@yahoo.com>
2017-02-08 16:39:33 +0100

    Added paging Added UnitTest for paging

--- SportsStore/SportsStore.Tests/UnitTest1.cs
+++ SportsStore/SportsStore.Tests/UnitTest1.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore.WebUI.Controllers;
+using System.Web.Mvc;
 
 namespace SportsStore.Tests
 {
@@ -7,8 +14,27 @@ namespace SportsStore.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Paging_4_Items_Per_Site()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }.AsQueryable);
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.pageSize = 3;
+
+            IQueryable<Product> result = (IQueryable<Product>)controller.List(2).Model;
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
         }
     }
 }
--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using SportsStore.Domain.Abstract;
+﻿using PagedList;
+using SportsStore.Domain.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,20 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int pageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        // GET: Home
-        public ActionResult List()
+        //paging 4 items per site
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
-        }
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize));
+        }    
     }
 }
