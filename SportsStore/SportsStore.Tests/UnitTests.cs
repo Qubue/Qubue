3b25f4b66083a5e70dda82dc0e851921ab811db8
Qubue <j.wojcicki@yahoo.com>
2017-02-08 17:21:21 +0100

    Added Pagging HtmlHelpers Added UnitTest for Paging

--- SportsStore/SportsStore.Tests/UnitTests.cs
+++ SportsStore/SportsStore.Tests/UnitTests.cs
@@ -7,15 +7,18 @@ using System.Collections.Generic;
 using System.Linq;
 using SportsStore.WebUI.Controllers;
 using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
         [TestMethod]
         public void Paging_4_Items_Per_Site()
         {
+            //creating object
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[]
             {
@@ -25,16 +28,38 @@ namespace SportsStore.Tests
                 new Product {ProductID = 4, Name = "P4"},
                 new Product {ProductID = 5, Name = "P5"}
             }.AsQueryable);
-
+            //creating controller
             ProductController controller = new ProductController(mock.Object);
             controller.pageSize = 3;
-
+            //getting data from model
             IQueryable<Product> result = (IQueryable<Product>)controller.List(2).Model;
 
             Product[] prodArray = result.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Proper_Generate_Page_Links()
+        {
+            HtmlHelper myHelper = null;
+            //creating new data for paginginfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            //preparing delegate
+            Func<int, string> PageUrlDelegate = i => "Strona" + i;
+
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, PageUrlDelegate);
+
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>" 
+            + @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Strona3"">3</a>", result.ToString());
         }
     }
 }
